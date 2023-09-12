dffb2518ed546796c5b3c0f5653d7b0139192f21
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-09-11 22:49:15 -0300

    update: updated the color and conditional renrering

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -8,29 +8,30 @@ export default function Header() {
   const signIn = false;
 
   return (
-    <header className={Style.C_header}>
-      <div className={Style.C_header_logo}>
-        <Link to="/">
-          <img src={logo} alt="logo da loja" />
-        </Link>
-      </div>
+    <div className={Style.container}>
+      <header className={Style.C_header}>
+        <div className={Style.C_header_logo}>
+          <Link to="/">
+            <img src={logo} alt="logo da loja" />
+          </Link>
+        </div>
 
-      {!isLoading && signIn && (
-        <Link to="/dashboard">
-          <div className={Style.C_header_logo_container}>
-            <FiUser className={Style.C_header_logo} />
-          </div>
-        </Link>
-      )}
+        {!isLoading && signIn && (
+          <Link to="/dashboard">
+            <div className={Style.C_header_logo_container}>
+              <FiUser className={Style.C_header_logo} />
+            </div>
+          </Link>
+        )}
 
-      {!isLoading && !signIn && (
-        <Link to="/login">
-          <div className={Style.C_header_logo_container}>
-            <FiLogIn className={Style.C_header_logo} />
-          </div>
-        </Link>
-      )}
-
-    </header>
+        {!isLoading && !signIn && (
+          <Link to="/login">
+            <div className={Style.C_header_logo_container}>
+              <FiLogIn className={Style.C_header_logo} />
+            </div>
+          </Link>
+        )}
+      </header>
+    </div>
   )
 }
