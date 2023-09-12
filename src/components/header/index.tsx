9df82d8e1d9409ef5b50b1f6bbc2edfda1b88982
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-09-11 22:29:26 -0300

    update: updated the color and conditional renrering

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,12 @@
 import logo from '../../assets/logo.svg';
 import Style from './header.module.css';
-import { LiaUserCircle } from 'react-icons/lia';
+import { FiUser, FiLogIn } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+  const isLoading = false;
+  const signIn = false;
+
   return (
     <header className={Style.C_header}>
       <div className={Style.C_header_logo}>
@@ -11,9 +14,23 @@ export default function Header() {
           <img src={logo} alt="logo da loja" />
         </Link>
       </div>
-      <Link to="/dashboard">
-        <LiaUserCircle size={45} className={Style.C_header_logo} />
-      </Link>
+
+      {!isLoading && signIn && (
+        <Link to="/dashboard">
+          <div className={Style.C_header_logo_container}>
+            <FiUser className={Style.C_header_logo} />
+          </div>
+        </Link>
+      )}
+
+      {!isLoading && !signIn && (
+        <Link to="/login">
+          <div className={Style.C_header_logo_container}>
+            <FiLogIn className={Style.C_header_logo} />
+          </div>
+        </Link>
+      )}
+
     </header>
   )
 }
