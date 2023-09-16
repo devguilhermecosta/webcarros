e702ffc52627f2fed8474a321734e0fd146ffa92
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-09-15 23:01:27 -0300

    update: updated the page

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,11 +1,22 @@
 import Container from "../../../components/container";
 import PanelHeader from "../../../components/panelheader";
+import Style from './new.module.css';
+import { FiUpload } from 'react-icons/fi';
 
 export default function New() {
   return (
     <Container>
       <PanelHeader />
-      <h1>Novo Carro</h1>
+      <div className={Style.C_image_gallery}>
+        <button className={Style.C_image_gallery_button}>
+          <div className={Style.C_image_gallery_button_icon}>
+            <FiUpload size={30}/>
+          </div>
+          <div>
+            <input type="file" accept="image/*" className={Style.C_image_gallery_button_input}/>
+          </div>
+        </button>
+      </div>
     </Container>
   )
 }
