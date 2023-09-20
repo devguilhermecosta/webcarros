95f144a92136abf863652aec1a191fa9fb71d38d
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-09-19 23:40:43 -0300

    feat: created the function for create the new car

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -2,13 +2,31 @@ import Container from "../../../components/container";
 import PanelHeader from "../../../components/panelheader";
 import Style from './new.module.css';
 import Input from "../../../components/input";
-import { FiUpload } from 'react-icons/fi';
+import { FiUpload, FiTrash } from 'react-icons/fi';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { ChangeEvent, useState, useContext } from 'react';
+import { storage, db } from '../../../services/FirabaseConnection';
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from 'firebase/storage'
+import { addDoc, collection } from 'firebase/firestore';
+import { AuthContext } from '../../../contexts/authcontext';
+import { v4 as uuidV4 } from 'uuid';
 
 
 type FormData = z.infer<typeof schema>;
+
+interface CarImageProps {
+  uid: string;
+  name: string;
+  previewUrl: string;
+  url: string;
+}
 
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatório"),
@@ -25,7 +43,8 @@ const schema = z.object({
 
 
 export default function New() {
-
+  const { user } = useContext(AuthContext);
+  const [ carImages, setCarImages ] = useState<CarImageProps[]>([]);
   const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
     resolver: zodResolver(schema),
     mode: "onChange"
@@ -33,7 +52,88 @@ export default function New() {
 
 
   function onSubmit(data: FormData) {
-    console.log(data);
+    if (carImages.length === 0) {
+      alert("Envie ao menos uma imagem!");
+      return;
+    }
+
+    const carImageList = carImages.map( carImage => {
+      return {
+        uid: carImage.uid,
+        name: carImage.name,
+        url: carImage.url
+      }
+    })
+
+    addDoc(collection(db, "cars"), {
+      owner: user?.name,
+      uid: user?.uid,
+      images: carImageList,
+      created_at: new Date(),
+      name: data.name,
+      model: data.model,
+      year: data.year,
+      km: data.km,
+      whatsapp: data.whatsapp,
+      city: data.city,
+      price: data.price,
+      description: data.description
+    })
+    .then( () => {
+      reset();
+      setCarImages([]);
+      console.log('cadastrado com sucesso');
+    })
+    .catch( (error) => {
+      console.log(`Erro ao enviar a imagem: ${error}`);
+    })
+  }
+
+  async function handleFile(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files && e.target.files[0]) {
+      const image = e.target.files[0];
+
+      if (image.type === 'image/jpeg' || image.type === 'image/png') {
+        await handleUpload(image);
+      } else {
+        alert('envie somente imagens JPEG ou PNG...');
+      }
+      
+    }
+  }
+
+  async function handleUpload(image: File) {
+    if (!user?.uid) return;
+
+    const currentUid = user?.uid;
+    const uidImage = uuidV4();
+
+    const uploadRef = ref(storage, `images/${currentUid}/${uidImage}`);
+
+    await uploadBytes(uploadRef, image)
+    .then((snapshot) => {
+      getDownloadURL(snapshot.ref)
+      .then((downloadUrl) => {
+        const imageItem = {
+          name: uidImage,
+          uid: currentUid,
+          previewUrl: URL.createObjectURL(image),
+          url: downloadUrl
+        }
+
+        setCarImages((images) => [...images, imageItem]);
+      })
+    })
+  }
+
+  async function handleDelete(image: CarImageProps) {
+    const imageRef = ref(storage, `images/${image.uid}/${image.name}`);
+    try {
+      await deleteObject(imageRef);
+      setCarImages(carImages.filter((carImage) => carImage.url !== image.url ));
+    } catch(e) {
+      console.log(e);
+    }
   }
 
   return (
@@ -45,9 +145,30 @@ export default function New() {
             <FiUpload size={30}/>
           </div>
           <div>
-            <input type="file" accept="image/*" className={Style.C_image_gallery_button_input}/>
+            <input
+              type="file"
+              accept="image/*" 
+              className={Style.C_image_gallery_button_input}
+              onChange={handleFile}
+            />
           </div>
         </button>
+
+        {carImages.map(item => (
+          <div key={item.name} className={Style.C_image}>
+            <button className={Style.C_image_button_delete}>
+              <FiTrash 
+                size={28} 
+                color="#fff" 
+                onClick={() => {handleDelete(item)}}
+              />
+            </button>
+            <img 
+              src={item.url} 
+              alt="car image"
+            />
+          </div>
+        ))}
       </div>
 
       <div className={Style.C_form}>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,71 @@
 import Container from "../../components/container";
 import Style from './home.module.css';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { db } from '../../services/FirabaseConnection';
+import { collection, query, getDocs, orderBy } from 'firebase/firestore';
+
+
+interface CarProps {
+  id: string;
+  owner: string;
+  uid: string;
+  created_at: string;
+  name: string;
+  model: string;
+  year: string;
+  km: string;
+  price: string | number;
+  whatsapp: string;
+  city: string;
+  images: ImageProps[];
+}
+
+interface ImageProps {
+  uid: string;
+  name: string;
+  url: string;
+}
 
 export default function Home() {
+  const [cars, setCars] = useState<CarProps[]>([]);
+
+  
+  useEffect(() => {
+    function loadCars() {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created_at", "desc"));
+
+      getDocs(queryRef)
+      .then((snapshot) => {
+        const carList = [] as CarProps[];
+
+        snapshot.forEach( doc => {
+          carList.push({
+            id: doc.id,
+            owner: doc.data().owner,
+            uid: doc.data().uid,
+            created_at: doc.data().created_at,
+            name: doc.data().name,
+            model: doc.data().model,
+            year: doc.data().year,
+            km: doc.data().km,
+            price: doc.data().price,
+            whatsapp: doc.data().whatsapp,
+            city: doc.data().city,
+            images: doc.data().images
+          })
+        })
+
+        setCars(carList);
+
+      })
+    }
+
+    loadCars();
+
+  }, [])
+
   return (
     <Container>
       <section className={Style.C_search}>
@@ -15,20 +79,27 @@ export default function Home() {
       <h1 className={Style.title}>Carros novos e usados em todo o Brasil</h1>
 
       <main className={Style.C_main}>
-        <section className={Style.C_main_car}>
-          <img src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202309/20230909/peugeot-208-1.6-griffe-16v-flex-4p-automatico-wmimagem09314018065.jpg?s=fill&w=552&h=414&q=60" alt="car" />
-          <div className={Style.C_main_car_details}>
-            <div className={Style.C_main_car_description}>
-              <p className={Style.C_main_car_details_name}>Nome do Carro</p>
-              <p className={Style.C_main_car_details_year}>Ano do Carro</p>
-              <p className={Style.C_main_car_details_price}><strong>R$ 100.000,00</strong></p>
-            </div>
 
-            <div className={Style.C_main_car_details_city_container}>
-              <p className={Style.C_main_car_details_city}>Dois Vizinhos - PR</p>
-            </div>
-          </div>
-        </section>
+        {cars.map( (car) => (
+          <Link key={car.id}  to={`/car/${car.id}`}>
+            <section className={Style.C_main_car}>
+              <img src={car.images[0].url} alt="car image" />
+              <div className={Style.C_main_car_details}>
+                <div className={Style.C_main_car_description}>
+                  <p className={Style.C_main_car_details_name}>{car.name}</p>
+                  <p className={Style.C_main_car_details_year}>{car.year}</p>
+                  <p className={Style.C_main_car_details_price}><strong>R$ {car.price}</strong></p>
+                </div>
+
+                <div className={Style.C_main_car_details_city_container}>
+                  <p className={Style.C_main_car_details_city}>{car.city}</p>
+                </div>
+              </div>
+            </section>
+          </Link>
+
+        ) )}
+  
       </main>
     </Container>
   )
